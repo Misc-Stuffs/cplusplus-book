b3660b518d4da63e72e69f91fdd1e1817e65ef73
Benjamin Worthington <benworthin@icloud.com>
2020-05-19 01:31:42 -0500

    Lesson one finished.

--- lesson-one/main.cpp
+++ lesson-one/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+// Listing 1.1
+//int main() {
+//    std::cout << "Hello, World!" << std::endl;
+//    return 0;
+//}
+
+// Exercise 1
+//int main() {
+//    int x = 8;
+//    int y = 6;
+//    std::cout << std::endl;
+//    std::cout << x - y << " " << x * y << " " << x + y;
+//    std::cout << std::endl;
+//    return 0;
+//}
+
+// Exercise 2
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello Buggy World \n";
     return 0;
 }
