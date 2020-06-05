a76fa3c91b07162061337ea0522c1999d2b28c65
Benjamin Worthington <benworthin@icloud.com>
2020-06-05 00:57:53 -0500

    Stopping point for lesson three (2).

--- lesson-three/main.cpp
+++ lesson-three/main.cpp
@@ -61,7 +61,7 @@ int main() {
 }
 */
 
-// Listing 3.3 - Using Global Variables
+/* Listing 3.3 - Using Global Variables
 #include <iostream>
 using namespace std;
 
@@ -98,6 +98,52 @@ int main() {
     cout << "Displaying from main(): ";
     cout << firstNumber << " x " << secondNumber;
     cout << " = " << multiplicationResult << endl;
+
+    return 0;
+
+}
+*/
+
+/* Listing 3.4 - Demonstrating Ill-Effects of Signed and Un-Signed Integer Overflow Errors
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    unsigned short uShortValue = 65535;
+    cout << "Incrementing unsigned short " << uShortValue << " gives: ";
+    cout << ++uShortValue << endl;
+
+    short signedShort = 32767;
+    cout << "Incrementing signed short " << signedShort << " gives: ";
+    cout << ++signedShort << endl;
+
+    return 0;
+
+}
+*/
+
+// Listing 3.5 - Finding the Size of Standard C++ Variable Types
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Computing the size of some C++ inbuilt variable types" << endl;
+
+    cout << "Size of bool: " << sizeof(bool) << endl;
+    cout << "Size of char: " << sizeof(char) << endl;
+    cout << "Size of unsigned short int: " << sizeof(unsigned short) << endl;
+    cout << "Size of short int: " << sizeof(short) << endl;
+    cout << "Size of unsigned long int: " << sizeof(unsigned long) << endl;
+    cout << "Size of long: " << sizeof(long) << endl;
+    cout << "Size of int: " << sizeof(int) << endl;
+    cout << "Size of unsigned long long: " << sizeof(unsigned long long) << endl;
+    cout << "Size of long long: " << sizeof(long long) << endl;
+    cout << "Size of unsigned int: " << sizeof(unsigned int) << endl;
+    cout << "Size of float: " << sizeof(float) << endl;
+    cout << "Size of double: " << sizeof(double) << endl;
+    cout << "The output changes with compiler, hardware and OS." << endl;
 
     return 0;
 
