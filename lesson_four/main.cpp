2b81d551b6ba96b2e25293d5fc1c464dd6e2c470
Benjamin Worthington <benworthin@icloud.com>
2020-06-09 20:38:12 -0500

    Lesson four stopping point.

--- lesson_four/main.cpp
+++ lesson_four/main.cpp
@@ -1,6 +1,17 @@
+// Listing 4.1 - Declaring an integer array and accessing its elements
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int myNumbers [5] = {7, 14, 21, 28, 35};
+
+    cout << "First element at index 0: " << myNumbers [0] << endl;
+    cout << "Second element at index 1: " << myNumbers [1] << endl;
+    cout << "Third element at index 2: " << myNumbers [2] << endl;
+    cout << "Fourth element at index 3: " << myNumbers [3] << endl;
+    cout << "Fifth element at index 4: " << myNumbers [4] << endl;
+
     return 0;
+
 }
