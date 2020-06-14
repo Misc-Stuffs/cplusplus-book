713cd4d4af30c65276b513546b7b6b2a14ffb571
Benjamin Worthington <benworthin@icloud.com>
2020-06-14 01:54:45 -0500

    Stopping point for Lesson four.

--- lesson-four/main.cpp
+++ lesson-four/main.cpp
@@ -17,7 +17,7 @@ int main() {
 }
 */
 
-// Listing 4.2 - Assigning Values to Elements in an Array
+/* Listing 4.2 - Assigning Values to Elements in an Array
 #include <iostream>
 using namespace std;
 constexpr int Square(int number) { return number * number; }
@@ -48,6 +48,62 @@ int main() {
 
     cout << "Element " << elementIndex << " in array moreNumbers is: ";
     cout << moreNumbers[elementIndex] << endl;
+
+    return 0;
+
+}
+*/
+
+/* Listing 4.3 - Accessing Elements in a Multidimensional Array
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int threeRowsThreeColumns [3] [3] = {{7, 14, 21}, {28, 35, 42}, {49, 56, 63}};
+
+    cout << "Row 0: " << threeRowsThreeColumns [0] [0] << " ";
+    cout << threeRowsThreeColumns [0] [1] << " ";
+    cout << threeRowsThreeColumns [0] [2] << " " << endl;
+
+    cout << "Row 1: " << threeRowsThreeColumns [1] [0] << " ";
+    cout << threeRowsThreeColumns [1] [1] << " ";
+    cout << threeRowsThreeColumns [1] [2] << " " << endl;
+
+    cout << "Row 2: " << threeRowsThreeColumns [2] [0] << " ";
+    cout << threeRowsThreeColumns [2] [1] << " ";
+    cout << threeRowsThreeColumns [2] [2] << " " << endl;
+
+    return 0;
+
+}
+*/
+
+// Listing 4.4 - Creating a Dynamic Array of Integers and Inserting Values Dynamically
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    // Dynamic array of type int
+    vector<int> dynArray (3);
+
+    dynArray[0] = 7;
+    dynArray[1] = 9;
+    dynArray[2] = 11;
+
+    cout << "Number of integers in array: " << dynArray.size() << endl;
+
+    cout << "Enter another element to insert into the dynamic array: " << endl;
+    int newDynArrayValue = 0;
+    cin >> newDynArrayValue;
+    dynArray.push_back(newDynArrayValue);
+
+    cout << "Number of integers in array: " << dynArray.size() << endl;
+    cout << "Last element in array: ";
+    cout << dynArray[dynArray.size() - 1] << endl;
 
     return 0;
 
